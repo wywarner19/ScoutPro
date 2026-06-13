c8bc09145b152be6d116fbf5bbcf613a77fcc65f
wywarner19 <wywarner19@gmail.com>
2026-06-13 18:37:20 -0500

    Add Vite configuration for React project

---
+++ Vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// base must match your repo name: /scoutpro/
+export default defineConfig({
+  plugins: [react()],
+  base: "/scoutpro/",
+});
